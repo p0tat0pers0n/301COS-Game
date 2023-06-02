168448d989c6116a2005b1e293ee22ed88d5ba49
p0tat0pers0n <100097047+p0tat0pers0n@users.noreply.github.com>
2023-06-02 12:31:22 +1200

    Player Movement
    Jumping, Changed gravity, started ground collision check

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,28 +7,43 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f; // Player movement speed
     public float rotationSensitivity = 2f; // Mouse rotation sensitivity
     public float rotationSmoothing = 0.1f; // Mouse rotation smoothing
-
+    public float jumpAcceleration = 5f;
     public Animator anim;
+    public Transform playerCamera;
+
     private Rigidbody rb;
+    private CapsuleCollider playerCollider;
     private Vector3 movementInput;
     public float rotationSpeed = 100f; // Player rotation speed
     private float mouseXSmooth = 0f;
     private float mouseYSmooth = 0f;
     private float mouseXVel = 0f;
     private float mouseYVel = 0f;
+    private float distToGround;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<CapsuleCollider>();
+        distToGround = playerCollider.bounds.extents.y;
     }
 
-    private void Update()
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, (float)(distToGround + 0.1));
+    }
+
+private void Update()
     {
         // Get player input
         movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         float rawMouseX = Input.GetAxis("Mouse X");
         float rawMouseY = Input.GetAxis("Mouse Y");
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += new Vector3(0f, jumpAcceleration, 0f);
+        }
 
         // Apply mouse smoothing
         mouseXSmooth = Mathf.SmoothDamp(mouseXSmooth, rawMouseX, ref mouseXVel, rotationSmoothing);
@@ -47,7 +62,10 @@ public class PlayerMovement : MonoBehaviour
         float yRotation = mouseYSmooth * rotationSpeed * Time.fixedDeltaTime;
 
         rb.MoveRotation(rb.rotation * Quaternion.Euler(Vector3.up * xRotation));
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(Vector3.right * yRotation));
+        // Rotate the camera only on the X-axis (up and down)
+        Vector3 cameraRotation = playerCamera.localEulerAngles;
+        cameraRotation.x -= yRotation;
+        playerCamera.localEulerAngles = cameraRotation;
 
         // Get input values
         float horizontalInput = Input.GetAxis("Horizontal");
