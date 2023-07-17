86b1efb44d11ba7ed16bd56e5cfb53d64108750f
p0tat0pers0n <100097047+p0tat0pers0n@users.noreply.github.com>
2023-07-17 13:31:51 +1200

    Attempted fix on playerstats GUI code

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -15,30 +15,38 @@ public class StatusBarScript : MonoBehaviour
     private RectTransform staminaTransform;
     private float healthStartingWidth;
     private float staminaStartingWidth;
+    private float healthStartingPosition;
+    private float staminaStartingPosition;
 
     void Start()
     {
         healthTransform = healthBar.GetComponent<RectTransform>();
         healthStartingWidth = healthTransform.rect.width;
+        healthStartingPosition = healthTransform.position.x;
 
         staminaTransform = staminaBar.GetComponent<RectTransform>();
         staminaStartingWidth = staminaTransform.rect.width;
+        staminaStartingPosition = staminaTransform.position.x;
 
         playerHealth = playerInitialHealth;
         playerStamina = playerInitialStamina;
+        Debug.Log(healthStartingPosition);
+        Debug.Log(((playerInitialHealth / healthStartingWidth) * playerHealth) / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     public void changePlayerHealth(float amount)
     {
+        Debug.Log("Health Transform Position.x:" + healthTransform.position.x);
+        Debug.Log("Health Size Change:" + (healthStartingWidth / playerInitialHealth) * playerHealth);
         playerHealth += amount;
         healthTransform.sizeDelta = new Vector2((healthStartingWidth / playerInitialHealth) * playerHealth, healthTransform.sizeDelta.y);
-        healthTransform.position = new Vector2(- healthTransform.position.x + (((healthStartingWidth / playerInitialHealth) * playerHealth) / 2), healthTransform.position.y);
+        healthTransform.position = new Vector2(healthStartingPosition - (healthStartingWidth - (healthStartingWidth / playerInitialHealth) * playerHealth)/2, healthTransform.position.y);
     }
 
     public void changePlayerStamina(float amount)
