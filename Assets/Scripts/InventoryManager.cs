7044a8c1f4236427ae5533e356a5a05299e7d603
p0tat0pers0n <100097047+p0tat0pers0n@users.noreply.github.com>
2023-07-28 12:29:53 +1200

    Started item management

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -9,10 +9,14 @@ public class InventoryManager : MonoBehaviour
     public Item potatoes;
     public GameObject hotbar;
 
+    private int activeSlot;
+    private GameObject hoe, shovel, wateringcan, potatoes;
+
     // Start is called before the first frame update
     void Start()
     {
         updateInventoryDisplay();
+        hoe = Instantiate(Resources.Load())
     }
 
     public void updateInventoryDisplay()
@@ -22,6 +26,55 @@ public class InventoryManager : MonoBehaviour
             GameObject slot = hotbar.transform.Find("HotbarItem" + i).gameObject;
             slot.transform.Find("Icon").gameObject.GetComponent<Image>().sprite = items[i].icon;
             slot.transform.Find("Name").gameObject.GetComponent<Text>().text = items[i].itemName;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            activeSlot = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            activeSlot = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            activeSlot = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            activeSlot = 4;
+        }
+    }
+
+    private void unEquipItems()
+    {
+        for (int i = 0; i<4; i++)
+        {
+            items[i].equipped = false;
+        }
+    }
+
+    private void holdItem()
+    {
+        switch(activeSlot)
+        {
+            case 1:
+                items[activeSlot].equipped = true;
+
+                break;
+            case 2:
+                items[activeSlot].equipped = true;
+
+                break;
+            case 3:
+                items[activeSlot].equipped = true;
+
+                break;
+            case 4:
+                potatoes.equipped = true;
+                break;
         }
     }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     public Sprite icon;
+    public GameObject model;
     public string itemName;
     public int amount;
     public int hotbarSlot;
