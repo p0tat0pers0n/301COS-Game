c5a2cb4978b0c0c6a125b6da85ffb27978726226
p0tat0pers0n <100097047+p0tat0pers0n@users.noreply.github.com>
2023-08-24 10:59:49 +1200

    Crops can be planted, started mandatory watering

--- Assets/Scripts/PlotController.cs
+++ Assets/Scripts/PlotController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlotController : MonoBehaviour
 {
     [SerializeField] private GameObject text;
+    [SerializeField] private GameObject interactText;
+    [SerializeField] private GameObject player;
 
     private int growthState, growthTime, requiredGrowthTime, intervalGrowth;
     private double fixedGrowthTime;
-    private bool planted;
+    private bool planted, withinRange, watered;
     private LayerMask mask;
     public List<Material> materials = new List<Material>(3);
     // Start is called before the first frame update
@@ -19,28 +22,58 @@ public class PlotController : MonoBehaviour
         fixedGrowthTime = 0;
         intervalGrowth = 5;
         planted = false;
+        watered = false;
         mask = LayerMask.GetMask("FarmPlot");
     }
-    // To start normal farm 
 
     private void OnMouseOver()
     {
-        Ray ray;
-        RaycastHit hit;
-
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
+        if (!planted || !watered)
         {
-            if (hit.distance <= 5)
-            {   
-                Debug.Log("Player Within Range");
+            Ray ray;
+            RaycastHit hit;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.distance <= 5)
+                {
+                    withinRange = true;
+                    if (!planted) { interactText.GetComponent<Text>().text = "Plant Potatoes"; }
+                    if (!watered) { interactText.GetComponent<Text>().text = "Water Crops"; }
+                }
             }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        withinRange = false;
+        interactText.GetComponent<Text>().text = "";
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && withinRange && !planted)
+        {
+            planted = true;
+            withinRange = false;
+            interactText.GetComponent<Text>().text = "";
+            gameObject.GetComponent<MeshRenderer>().material = materials[0];
+        }
+
+        if (Input.GetMouseButtonDown(0) && withinRange && !watered)
+        {
+            /*if (player)
+            {
+
+            }*/
         }
     }
 
     void FixedUpdate()
     {
-        if (planted)
+        if (planted && watered)
         {
             fixedGrowthTime++;
             growthTime = (int)fixedGrowthTime / 50;
